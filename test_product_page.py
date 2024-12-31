d07bd78bc7f69111fed08c7207f975eabef11d58
Kira <akw98@ya.ru>
2024-12-31 16:38:26 +0500

    Product page test. Added parameterization and xfail tag.

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,12 @@
+import pytest
 from .pages.product_page import ProductPage
 from time import sleep
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize('promo_offer', ['0', '1', '2', '3', '4', '5', '6', 
+                                         pytest.param('7', marks = pytest.mark.xfail), 
+                                         '8', '9'])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}'
     prod_page = ProductPage(browser, link)
     prod_page.open()
     prod_page.add_to_basket()
